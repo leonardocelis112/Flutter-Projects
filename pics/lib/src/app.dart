786849c67e7d4c968af00dfd0f1b29910fa5c1f5
Leonardo Celis <leonardocelis112@gmail.com>
2021-02-07 21:31:53 -0500

    Add images and network calls

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,5 +1,9 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
 // Create a class that will be our custom widget
 // Must extend stateless class
@@ -14,6 +18,18 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +39,10 @@ class AppState extends State<App> {
           title: Text("Let's see some images!"),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
           child: Icon(Icons.add),
         ),
-        body: Text('$counter'),
+        body: ImageList(images),
       ),
     );
     ;
