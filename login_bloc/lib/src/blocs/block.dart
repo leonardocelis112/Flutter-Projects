e7243d93bcb81646f09a874f66a2ae1e7e2390b3
Leonardo Celis <leonardocelis112@gmail.com>
2021-05-05 21:52:20 -0500

    Finish the block patter implementation

--- login_bloc/lib/src/blocs/block.dart
+++ login_bloc/lib/src/blocs/block.dart
@@ -1,15 +1,23 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc extends Object with Validators {
-  final _email = StreamController<String>();
-  final _password = StreamController<String>();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
 
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid =>
+      Observable.combineLatest2(email, password, (email, password) => true);
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  submit() {
+    final validEmail = this._email.value;
+    final validPassword = this._password.value;
+  }
 
   dispose() {
     _email.close();
